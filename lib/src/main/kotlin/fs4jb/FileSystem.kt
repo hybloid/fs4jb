4e2959741d22beafc7a636b137db5e23fe8344f8
Dmitry Golovinov <zobber@bk.ru>
2022-04-12 12:38:41 +0200

    Fix naming issues

--- lib/src/main/kotlin/fs4jb/FileSystem.kt
+++ lib/src/main/kotlin/fs4jb/FileSystem.kt
@@ -77,10 +77,10 @@ class FileSystem(private val disk: Disk) {
         // FIXME: we do not check . and .. here
         assert(!name.contains(Constants.DELIMITER))
         assert(name.length <= Constants.FILENAME_SIZE)
-        val bufToAppend = ByteBuffer.allocate(Constants.DENTRY_SIZE)
-        bufToAppend.put(wrapDentryName(name))
-        bufToAppend.putInt(source.number)
-        write(target, bufToAppend, target.size, bufToAppend.limit())
+        val buffer = ByteBuffer.allocate(Constants.DENTRY_SIZE)
+        buffer.put(wrapNameForDentry(name))
+        buffer.putInt(source.number)
+        write(target, buffer, target.size, buffer.limit())
     }
 
     fun removeFromDir(source: INode, target: INode) {
@@ -116,13 +116,12 @@ class FileSystem(private val disk: Disk) {
         assert(dentries >= 2)
         val targetBuf = ByteBuffer.allocate(target.size) // Expensive
         read(target, targetBuf, 0, target.size)
-        val bufferForNextName = ByteArray(Constants.FILENAME_SIZE)
-        val bufWithNameToCompare = wrapDentryName(name)
-        val arrayWithTargetName = bufWithNameToCompare.array()
+        val dentryName = ByteArray(Constants.FILENAME_SIZE)
+        val targetName = wrapNameForDentry(name).array()
         for (i in 0 until dentries) {
             targetBuf.position(i * Constants.DENTRY_SIZE)
-            targetBuf.get(bufferForNextName)
-            if (arrayWithTargetName.contentEquals(bufferForNextName)) {
+            targetBuf.get(dentryName)
+            if (targetName.contentEquals(dentryName)) {
                 val num = targetBuf.getInt(i * Constants.DENTRY_SIZE + Constants.FILENAME_SIZE)
                 return retrieveINode(num)
             }
@@ -137,10 +136,10 @@ class FileSystem(private val disk: Disk) {
         val list = mutableListOf<Pair<String, Int>>()
         val targetBuf = ByteBuffer.allocate(target.size) // Expensive
         read(target, targetBuf, 0, target.size)
-        val bufferForName = ByteArray(Constants.FILENAME_SIZE)
+        val dentryName = ByteArray(Constants.FILENAME_SIZE)
         for (i in 0 until dentries) {
-            targetBuf.get(bufferForName)
-            val name = String(trim(bufferForName), Constants.CHARSET)
+            targetBuf.get(dentryName)
+            val name = String(trim(dentryName), Constants.CHARSET)
             val num = targetBuf.int
             list.add(Pair(name, num))
         }
@@ -166,9 +165,9 @@ class FileSystem(private val disk: Disk) {
     fun createINode(): INode {
         assert(freeInodes.size > 0)
         val buf = Constants.zeroBlock()
-        val nextNumber = freeInodes.removeFirst()
-        disk.read(INode.getBlockNumber(nextNumber), buf)
-        val inode = INode.read(nextNumber, buf)
+        val freeInodeNumber = freeInodes.removeFirst()
+        disk.read(INode.getBlockNumber(freeInodeNumber), buf)
+        val inode = INode.read(freeInodeNumber, buf)
         inode.valid = true
         // TODO : let's try to postpone writing this inode
         return inode
@@ -204,28 +203,28 @@ class FileSystem(private val disk: Disk) {
         val endBlockId = end / Constants.BLOCK_SIZE
         val endBlockPosition = end.mod(Constants.BLOCK_SIZE)
 
-        var readen = 0
+        var readCount = 0
         disk.read(inode.links[startBlockId], buf)
         if (startBlockId == endBlockId) {
             // special case
             val lengthToRead = endBlockPosition - startBlockPosition + 1
             buffer.put(buf.array(), startBlockPosition, lengthToRead)
-            readen += lengthToRead
+            readCount += lengthToRead
         } else {
             val startPartLength = Constants.BLOCK_SIZE - startBlockPosition
             val endPartLength = endBlockPosition + 1
             buffer.put(buf.array(), startBlockPosition, startPartLength)
-            readen += startPartLength
+            readCount += startPartLength
             for (i in startBlockId + 1 until endBlockId) {
                 disk.read(inode.links[i], buf)
                 buffer.put(buf)
-                readen += Constants.BLOCK_SIZE
+                readCount += Constants.BLOCK_SIZE
             }
             disk.read(inode.links[endBlockId], buf)
             buffer.put(buf.array(), 0, endPartLength)
-            readen += endPartLength
+            readCount += endPartLength
         }
-        assert(readen == length)
+        assert(readCount == length)
         buffer.flip()
         return length
     }
@@ -236,6 +235,8 @@ class FileSystem(private val disk: Disk) {
         assert(start >= 0)
         assert(end < Constants.INODE_TOTAL_LINKS_COUNT * Constants.BLOCK_SIZE)
         assert(buffer.capacity() >= length)
+        buffer.rewind()
+
         var buf = Constants.zeroBlock()
         if (inode.indirectLoadNeeded()) {
             // dummy safety, already done by retrieve node
@@ -266,33 +267,33 @@ class FileSystem(private val disk: Disk) {
             }
         }
         disk.read(inode.links[startBlockId], buf)
+
         buf.position(startBlockPosition)
-        buffer.rewind()
-        var written = 0
+        var writeCount = 0
         if (startBlockId == endBlockId) {
             // special case
             buf.put(buffer)
             assert(buf.position() == endBlockPosition + 1)
             disk.write(inode.links[startBlockId], buf)
-            written = buffer.limit()
+            writeCount = buffer.limit()
         } else {
             val startPartLength = Constants.BLOCK_SIZE - startBlockPosition
             val endPartLength = endBlockPosition + 1
             buf.put(buffer.array(), 0, startPartLength)
             disk.write(inode.links[startBlockId], buf)
-            written += startPartLength
+            writeCount += startPartLength
             buf.clear()
             for (i in startBlockId + 1 until endBlockId) {
-                buf.put(buffer.array(), written, Constants.BLOCK_SIZE)
+                buf.put(buffer.array(), writeCount, Constants.BLOCK_SIZE)
                 disk.write(inode.links[i], buf)
-                written += Constants.BLOCK_SIZE
+                writeCount += Constants.BLOCK_SIZE
                 buf.clear()
             }
-            buf.put(buffer.array(), written, endPartLength)
+            buf.put(buffer.array(), writeCount, endPartLength)
             disk.write(inode.links[endBlockId], buf)
-            written += endPartLength
+            writeCount += endPartLength
         }
-        assert(written == length)
+        assert(writeCount == length)
         // UPDATE INODE SIZE
         if (inode.size < end + 1) {
             inode.size = end + 1
@@ -313,15 +314,15 @@ class FileSystem(private val disk: Disk) {
             disk.read(inode.indirect, buf)
             inode.readIndirect(buf)
         }
-        val blockId = offset / Constants.BLOCK_SIZE
+        val startBlockId = offset / Constants.BLOCK_SIZE
         val lastBlockId = inode.size / Constants.BLOCK_SIZE
-        for (i in lastBlockId downTo blockId + 1) {
+        for (i in lastBlockId downTo startBlockId + 1) {
             if (inode.links[i] != 0) {
                 freeDataBlocks.addFirst(inode.links[i])
                 inode.links[i] = 0
             }
         }
-        if (blockId < Constants.LINKS_IN_INODE && inode.indirect != 0) {
+        if (startBlockId < Constants.LINKS_IN_INODE && inode.indirect != 0) {
             freeDataBlocks.addFirst(inode.indirect)
             inode.indirect = 0
         }
@@ -333,12 +334,12 @@ class FileSystem(private val disk: Disk) {
             }
         } else {
             val positionInLastBlock = offset.mod(Constants.BLOCK_SIZE)
-            disk.read(inode.links[blockId], buf)
+            disk.read(inode.links[startBlockId], buf)
             buf.position(positionInLastBlock)
             for (i in positionInLastBlock until Constants.BLOCK_SIZE) {
                 buf.put(0)
             }
-            disk.write(inode.links[blockId], buf)
+            disk.write(inode.links[startBlockId], buf)
         }
         inode.size = offset
         writeInodeToDisk(inode, buf)
@@ -354,11 +355,11 @@ class FileSystem(private val disk: Disk) {
         }
     }
 
-    private fun wrapDentryName(name: String): ByteBuffer {
-        val bufWithNameToCompare = ByteBuffer.allocate(Constants.FILENAME_SIZE)
-        bufWithNameToCompare.put(name.toByteArray(Constants.CHARSET))
-        bufWithNameToCompare.rewind()
-        return bufWithNameToCompare
+    private fun wrapNameForDentry(name: String): ByteBuffer {
+        val buffer = ByteBuffer.allocate(Constants.FILENAME_SIZE)
+        buffer.put(name.toByteArray(Constants.CHARSET))
+        buffer.rewind()
+        return buffer
     }
 
     private fun trim(bytes: ByteArray): ByteArray {
@@ -378,16 +379,16 @@ class FileSystem(private val disk: Disk) {
             disk.read(i, buf)
             for (j in 0 until Constants.INODES_PER_BLOCK) {
                 val number = i * Constants.INODES_PER_BLOCK + j
-                val iNode = INode.read(number, buf)
-                if (iNode.valid) {
-                    if (iNode.indirect != 0) {
-                        disk.read(iNode.indirect, indirectBuf)
-                        iNode.readIndirect(indirectBuf)
-                        busyBlocks.add(iNode.indirect)
+                val inode = INode.read(number, buf)
+                if (inode.valid) {
+                    if (inode.indirect != 0) {
+                        disk.read(inode.indirect, indirectBuf)
+                        inode.readIndirect(indirectBuf)
+                        busyBlocks.add(inode.indirect)
                     }
                     for (k in 0..Constants.INODE_TOTAL_LINKS_COUNT) {
-                        if (iNode.links[k] != 0) {
-                            busyBlocks.add(iNode.links[k])
+                        if (inode.links[k] != 0) {
+                            busyBlocks.add(inode.links[k])
                         } else {
                             break // TODO : can be problematic in case of error in algorithm. Add assert and test
                         }
