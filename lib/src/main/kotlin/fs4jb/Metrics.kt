01dec2ed595883a93e281dbef482c96e244f6cc8
Dmitry Golovinov <zobber@bk.ru>
2022-04-13 11:19:28 +0200

    - Add statistics to functional test

--- lib/src/main/kotlin/fs4jb/Metrics.kt
+++ lib/src/main/kotlin/fs4jb/Metrics.kt
@@ -1,15 +1,51 @@
 package fs4jb
 
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
+
 class Metrics {
     companion object {
-        var reads = 0
-        var writes = 0
+        var lowLevelReads = 0
+        var lowLevelWrites = 0
+        var fileReads = 0
+        var fileReadsTotalSpeed: Float = 0.0F
+        var fileWrites = 0
+        var fileWritesTotalSpeed: Float = 0.0F
 
-        fun incRead() = reads++
-        fun incWrite() = writes++
+        fun incRead() = lowLevelReads++
+        fun incWrite() = lowLevelWrites++
+
+        @OptIn(ExperimentalTime::class)
+        fun logReadSpeed(size: Int, time: Duration) {
+            fileReads++
+            fileReadsTotalSpeed += (size.toFloat() / time.inWholeMicroseconds)
+        }
+
+        fun readSpeed() = if (fileReads == 0 || fileReadsTotalSpeed == 0.0F) {
+            0
+        } else {
+            fileReadsTotalSpeed / fileReads * 1_000_000 / 1024
+        }
+
+        @OptIn(ExperimentalTime::class)
+        fun logWriteSpeed(size: Int, time: Duration) {
+            fileWrites++
+            fileWritesTotalSpeed += (size.toFloat() / time.inWholeMicroseconds)
+        }
+
+        fun writeSpeed() = if (fileWrites == 0 || fileWritesTotalSpeed == 0.0F) {
+            0
+        } else {
+            fileWritesTotalSpeed / fileWrites * 1_000_000 / 1024
+        }
+
         fun reset() {
-            reads = 0
-            writes = 0
+            lowLevelReads = 0
+            lowLevelWrites = 0
+            fileReads = 0
+            fileReadsTotalSpeed = 0.0F
+            fileWrites = 0
+            fileWritesTotalSpeed = 0.0F
         }
     }
 }
