8e05fa21f1a4633fa4ad7e8716f26619ea03a66b
Dmitry Golovinov <zobber@bk.ru>
2022-04-13 13:18:59 +0200

    Provide better implementation for LRU cache

--- lib/src/main/kotlin/fs4jb/Disk.kt
+++ lib/src/main/kotlin/fs4jb/Disk.kt
@@ -7,11 +7,16 @@ import java.nio.file.Path
 
 class Disk(private val path: Path, val nBlocks: Int) {
     private lateinit var channel: FileChannel
-    private val cache = LinkedHashMap<Int, ByteArray>()
+    private val cache = LRU()
+    private val cacheRoutine = { idx: Any, buf: Any ->
+        writeEntryToDisk(
+            idx as Int,
+            buf as ByteArray
+        )
+    }
 
     fun open(recreate: Boolean = false) {
         Metrics.reset()
-        cache.clear()
         channel = when (recreate) {
             true -> FileChannel.open(path, READ, WRITE, CREATE, TRUNCATE_EXISTING)
             else -> FileChannel.open(path, READ, WRITE, CREATE)
@@ -19,7 +24,8 @@ class Disk(private val path: Path, val nBlocks: Int) {
     }
 
     fun close() {
-        dumpCacheToDisk()
+        cache.iterate(cacheRoutine)
+        cache.clear()
         channel.force(true)
         channel.close()
     }
@@ -37,27 +43,23 @@ class Disk(private val path: Path, val nBlocks: Int) {
     fun read(blockNum: Int, buffer: ByteBuffer): Int {
         if (blockNum >= nBlocks || buffer.capacity() != Constants.BLOCK_SIZE) throw FSArgumentsException("Incorrect arguments")
         // read element from cache if it is there
-        val element = cache.get(blockNum)
+        val element = cache.get(blockNum) as ByteArray?
         if (element != null) {
             buffer.clear()
             buffer.put(element)
             buffer.flip()
-            cache[blockNum] = element
             return element.size
         }
 
         val count = channelRead(buffer, blockOffset(blockNum))
         if (count != Constants.BLOCK_SIZE) throw FSIOException("IO error while reading")
-        checkIfCacheShouldBeWritten()
-        cache[blockNum] = buffer.array().clone()
+        cache.put(blockNum, buffer.array().clone(), cacheRoutine)
         return count
     }
 
     fun write(blockNum: Int, buffer: ByteBuffer): Int {
         if (blockNum >= nBlocks || buffer.capacity() != Constants.BLOCK_SIZE) throw FSArgumentsException("Incorrect arguments")
-        cache.remove(blockNum)
-        checkIfCacheShouldBeWritten()
-        cache[blockNum] = buffer.array().clone()
+        cache.put(blockNum, buffer.array().clone(), cacheRoutine)
         return buffer.array().size
     }
 
@@ -79,20 +81,8 @@ class Disk(private val path: Path, val nBlocks: Int) {
         return channel.write(buffer, offset) // do not clear, since data could be reused (i.e. empty block)
     }
 
-    private fun checkIfCacheShouldBeWritten() {
-        if (cache.size >= Constants.LRU_CACHE_LIMIT) {
-            val last = cache.entries.last()
-            val cacheWrite = channelWrite(ByteBuffer.wrap(last.value), blockOffset(last.key))
-            if (cacheWrite != Constants.BLOCK_SIZE) throw FSIOException("IO error while writing")
-            cache.remove(cache.entries.last().key)
-        }
-    }
-
-    private fun dumpCacheToDisk() {
-        for ((num, buf) in cache.entries) {
-            val cacheWrite = channelWrite(ByteBuffer.wrap(buf), blockOffset(num))
-            if (cacheWrite != Constants.BLOCK_SIZE) throw FSIOException("IO error while writing")
-        }
-        cache.clear()
+    private fun writeEntryToDisk(idx: Int, data: ByteArray) {
+        val cacheWrite = channelWrite(ByteBuffer.wrap(data), blockOffset(idx))
+        if (cacheWrite != Constants.BLOCK_SIZE) throw FSIOException("IO error while writing")
     }
 }
