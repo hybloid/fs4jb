0b9d4b2b957e84c7ed6654c2a3bcc061420bdca7
Dmitry Golovinov <zobber@bk.ru>
2022-04-13 15:29:44 +0200

    Fix Java and Kotlin compatability issue

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -14,6 +14,17 @@ plugins {
     `java-library`
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
 repositories {
     // Use Maven Central for resolving dependencies.
     mavenCentral()
